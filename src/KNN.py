62eccc0cb4304a96f3327053804939ccb71013ab
arghavan99 <a.rezvani99@yahoo.com>
2019-08-22 10:08:39 +0430

    knn

--- src/KNN.py
+++ src/KNN.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def KNN_classifier(df, k, target, train_attr):
-    y_train, y_test, x_train, x_test = train_test_split(df[train_attr],
+    x_train, x_test, y_train, y_test = train_test_split(df[train_attr],
                                                         df[target], random_state=0, test_size=0.05)
     print("X_train shape: {}\ny_train shape: {}".format(x_train.shape, y_train.shape))
     print("X_test shape: {}\ny_test shape: {}".format(x_test.shape, y_test.shape))
